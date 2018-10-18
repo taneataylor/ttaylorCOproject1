6c1f4a7974e765aac79fdfae8f454cd3845ad639
taneataylor <taylor_tanea@yahoo.com>
2018-10-17 23:03:08 -0400

    Updates
    Finished id number, prints new line and beginning of last name

--- ttaylorProject1.asm
+++ ttaylorProject1.asm
@@ -1,10 +1,10 @@
 .text
   main:
-	li $v0, 11
+	li $v0, 11  # Prints chars
 	la $a0, 64   #@
 	syscall
 	
-	li $v1, 1
+	li $v1, 1  # Prints ints
 	addi $a0, $a0, -16 # 0 (48)
 	syscall
 	addi $a0, $a0, 2 # 2 (50)
@@ -17,5 +17,15 @@
 	syscall
 	addi $a0, $a0, 3 # 5 (53)
 	syscall
-	addi $a0, $a0, -3 # 2 (50)
+	addi $a0, $a0, -1 # 4 (52)
+	syscall
+	addi $a0, $a0, 0 # 4 (52)
+	syscall
+	
+	li $v0, 11  # Printing new line char
+	addi $a0, $a0, -42 # LF (10)
+	syscall
+	
+	li $v0, 10
+	addi $a0, $a0, 74 # T (84)
 	syscall
